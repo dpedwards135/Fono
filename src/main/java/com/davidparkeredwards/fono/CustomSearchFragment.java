76021046c775d2b6e82cbd3c008a054fe628c45d
Dave Edwards <dpedwards135@gmail.com>
2016-08-18 12:04:35 -0400

    Custom Search Functioning correctly

--- src/main/java/com/davidparkeredwards/fono/CustomSearchFragment.java
+++ src/main/java/com/davidparkeredwards/fono/CustomSearchFragment.java
@@ -12,6 +12,7 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -19,16 +20,21 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import com.davidparkeredwards.fono.data.EventDbManager;
 import com.davidparkeredwards.fono.data.EventsContract;
 
+import java.util.Date;
+
+// Next - Create input fields that store values to be passed to EventRequest - Keywords, date, location
 
 public class CustomSearchFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor> {
 
     private static final int EVENTS_LOADER = 0;
-
+    private View rootView;
 
     private EventsAdapter eventsAdapter;
 
@@ -39,6 +45,7 @@ public class CustomSearchFragment extends Fragment implements LoaderManager.Load
         }
     };
 
+    DatePicker datePicker;
     public CustomSearchFragment() {}
 
     @Override
@@ -59,7 +66,7 @@ public class CustomSearchFragment extends Fragment implements LoaderManager.Load
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         eventsAdapter = new EventsAdapter(getActivity(), null, 0);
 
-        View rootView = inflater.inflate(R.layout.fragment_custom_search, container, false);
+        rootView = inflater.inflate(R.layout.fragment_custom_search, container, false);
 
         ListView listView = (ListView) rootView.findViewById(R.id.custom_search_list_view);
 
@@ -76,16 +83,57 @@ public class CustomSearchFragment extends Fragment implements LoaderManager.Load
             }
         });
 
+        //Set onClickListener to Search  Button
         Button customSearchButton = (Button) rootView.findViewById(R.id.customSearchButton);
         customSearchButton.setOnClickListener(customSearchListener);
 
+        //Set MinDate to DatePicker
 
+        datePicker =  ((DatePicker) rootView.findViewById(R.id.customDatePicker));
+        datePicker.setMinDate(System.currentTimeMillis() - 1000);
+        datePicker.setSpinnersShown(true);
+        datePicker.setCalendarViewShown(false);
         return rootView;
 
     }
 
     public void customEventRequest() {
-        EventRequest customEventRequest = new EventRequest(getContext(), "", "", EventDbManager.CUSTOM_SEARCH_REQUEST, "Salt Lake");
+
+
+        //Format date for JSON = YYYYMMDD00-YYYYMMDD00
+        int day =datePicker.getDayOfMonth();
+        int month = datePicker.getMonth() + 1;
+        int year = datePicker.getYear();
+        String dayString = Integer.toString(day);
+        String monthString = Integer.toString(month);
+        String yearString = Integer.toString(year);
+
+        String customDate;
+        if (day < 10 && month < 10) {
+            customDate = "" + yearString + "0" + monthString +
+                    "0" + dayString + "00";
+        } else if(day >= 10 && month < 10) {
+            customDate = "" + yearString + "0" + monthString  + dayString + "00";
+        } else if(day < 10 && month >= 10) {
+            customDate = "" + yearString + monthString + "0" + dayString + "00";
+        }
+        else {
+            customDate = "" + yearString + monthString + dayString + "00";
+        }
+        customDate = customDate + "-" + customDate;
+
+
+        //Get Keywords and Location
+        String customKeywords = ((EditText) rootView.findViewById(R.id.customKeywords)).getText().toString();
+        String customLocation = ((EditText) rootView.findViewById(R.id.customLocation)).getText().toString();
+        Log.i("customEventRequest", "customEventRequest: " +
+                "Year: " + yearString +
+                "Month: " + monthString +
+                "Day: " + dayString
+        );
+        Log.i("customEventRequest", customDate + " " + customKeywords + " " + customLocation);
+        EventRequest customEventRequest = new EventRequest(getContext(), customDate, customKeywords,
+                EventDbManager.CUSTOM_SEARCH_REQUEST, customLocation);
         customEventRequest.execute();
     }
 
